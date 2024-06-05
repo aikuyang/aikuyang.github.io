64d5c6bea016837b07bc5b7af678c5163f356f6a
aikuyang <aikuyang@gmail.com>
2024-06-05 16:36:37 +0800

    d

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "760831150"
+    serviceWorkerVersion: "3101412945"
   }
 });
